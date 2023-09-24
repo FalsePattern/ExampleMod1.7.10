31efeb8b39fbfb19a3c17743e9ae8c9b6218d58f
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2023-09-24 13:45:21 +0200

    [ci skip] Proper nix fix for openal crashes in dev

--- shell.nix
+++ shell.nix
@@ -1,7 +1,22 @@
 { pkgs ? import <nixpkgs> { }, lib ? pkgs.lib }:
-pkgs.mkShell rec {
-  name = "minecraft-1.7.10-dev";
-  buildInputs = with pkgs; (
+let
+  openal64 = { pkgs, ...}:
+  let
+    stdenv = pkgs.stdenv;
+    openal = pkgs.openal;
+  in stdenv.mkDerivation rec {
+    pname = "openal64";
+    version = openal.version;
+    dontUnpack = true;
+    buildInputs = [ openal ];
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out/lib
+      ln -s ${openal}/lib/libopenal.so $out/lib/libopenal64.so
+      runHook postInstall
+    '';
+  };
+  runtimeLibs = with pkgs; (
     [
       temurin-bin-8
     ]
@@ -13,14 +28,30 @@ pkgs.mkShell rec {
       libXxf86vm
     ])
     ++ [
+      # lwjgl
       libpulseaudio
       libGL
-      glfw
       openal
+      (callPackage openal64 { inherit pkgs; })
+      glfw
       stdenv.cc.cc.lib
+
+      # oshi
+      udev
     ]
   );
-  LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+  runtimePrograms = with pkgs; [
+    xorg.xrandr
+    mesa-demos # need glxinfo
+  ];
+in
+pkgs.mkShell rec {
+  name = "minecraft-1.7.10-dev";
+  buildInputs = runtimeLibs ++ runtimePrograms;
+  shellHook = ''
+    export LD_LIBRARY_PATH=/run/opengl-driver/lib:${lib.makeLibraryPath runtimeLibs}:$LD_LIBRARY_PATH
+    export PATH=$PATH:${lib.makeBinPath runtimePrograms}
+  '';
   XDG_DATA_DIRS = builtins.getEnv "XDG_DATA_DIRS";
   XDG_RUNTIME_DIR = builtins.getEnv "XDG_RUNTIME_DIR";
 }
